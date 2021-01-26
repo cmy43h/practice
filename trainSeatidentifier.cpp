db162383f1364827b02bff5367a4e31bcd0c425e
fwiz43 <majumdarchinmay9@gmail.com>
2021-01-26 20:57:50 +0530

    adding train seat identifier programme

--- trainSeatidentifier.cpp
+++ trainSeatidentifier.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include<iostream>
+using namespace std;
+int main(){
+    int t,n,p;
+    cin>>t;
+    do {
+        cin>>n;
+        p=n/12;
+        n%=12;
+        if(n%3==2||n==2){
+            cout<<p*12+(13-n)<<" "<<"MS"<<endl;
+        }
+        else if(n%6==0||n%6==1||n==1){
+            n==0?cout<<p*12-11:cout<<p*12+(13-n);
+            cout<< " "<<"WS"<<endl;
+        }else cout<<p*12+(13-n)<<" "<<"AS"<<endl;
+        t--;
+    }while(t>0);
     return 0;
 }
